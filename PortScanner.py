e77776a8c2373982749f0c3b8903227c77a2df87
Ranjan Soumya <83495544+ranjansmy@users.noreply.github.com>
2021-11-14 17:51:43 +0530

    Update PortScanner.py

--- PortScanner.py
+++ PortScanner.py
@@ -1,6 +1,6 @@
 import socket
 from IPy import IP
-
+'''
 ipaddress = print(input('[+] Enter Target to Scan: '))
 port = 80
 
@@ -10,3 +10,18 @@ try:
         print('[+] Port 80 is Open ')
 except:
         print('[-] Port 80 is Closed ')
+'''
+
+def scanport(ipaddress, port):
+    try:
+        sock = socket.socket()
+        sock.settimeout(2)                              # 2sec/port
+        sock.connect((ipaddress, port))
+        print('[+] Port' + str(port) + ' is OPEN')      # port is stringfied, default Integer
+    except:
+        print('[-] Port' + str(port) + ' is CLOSED')
+
+ipaddress = print(input('Enter a target to scan: '))
+
+for port in range (1, 1024):
+    scanport(ipaddress, port)
