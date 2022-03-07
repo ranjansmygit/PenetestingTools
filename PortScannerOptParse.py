905f18b02d3bfff79320d11d06bffd4f17718597
Ranjan Soumya <83495544+ranjansmy@users.noreply.github.com>
2022-03-07 20:26:33 +0530

    Create PortScannerOptParse.py

---
+++ PortScannerOptParse.py
@@ -0,0 +1,32 @@
+import optparse
+from socket import *
+
+def connScan(tgtHost, tgtPort):
+    try:
+        connSkt = socket(AF_INET, SOCK_STREAM) 
+        connSkt.connect((tgtHost, tgtPort)) 
+        print ('[+]%d/tcp open'% tgtPort connSkt.close())
+    except:
+        print ('[-]%d/tcp closed'% tgtPort)
+
+def portScan(tgtHost, tgtPorts): 
+    try:
+        tgtIP = gethostbyname(tgtHost) 
+    except:
+        print ("[-] Cannot resolve '%s': Unknown host"%tgtHost)
+        return 
+    try:
+        tgtName = gethostbyaddr(tgtIP)
+        print ('\n[+] Scan Results for: ' + tgtName[0]) 
+    except:
+        print ('\n[+] Scan Results for: ' + tgtIP) 
+    setdefaulttimeout(1)
+
+    for tgtPort in tgtPorts:
+        print ('Scanning port ' + tgtPort connScan(tgtHost, int(tgtPort)))
+        
+"""
+The portScan() takes the hostname and target ports as arguments. 
+Next, it will print the hostname (or IP address) and enumerate through each individual port attempting to connect using the 
+connScan().
+"""
